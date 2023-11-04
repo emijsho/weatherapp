3bb2d676051ee76daa3df7dd3ab6ae071ac7c5c0
emijsho <145356364+emijsho@users.noreply.github.com>
2023-11-04 21:18:24 +0000

    added local time code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function showWeatherDetails(response) {
   console.log(response);
+  console.log(response.data.name);
   document.querySelector(
     "#location"
   ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
@@ -14,14 +15,32 @@ function showWeatherDetails(response) {
   document.querySelector("#celcius-link").innerHTML = `${Math.round(
     response.data.main.temp
   )}°C`;
+  let localTime = new Date(response.data.time * 1000);
+  console.log(localTime);
+  document.querySelector(
+    "#local-timeDay"
+  ).innerHTML = `Local Time: ${localTime.getHours()}:${localTime.getMinutes()}`;
+}
+
+function updateIcon() {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = "metric";
+  let apiKey = "494f3181eb1oe9bfae0t4f2214913d5b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  document.querySelector(
+    "#weatherIcon"
+  ).innerHTML = `<img src="${response.data.condition.icon_url}">`;
+  axios.get(apiUrl).then(showWeatherDetails);
 }
 function showPosition(position) {
   console.log(position);
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let units = "metric";
-  let apiKey = "3dce9b1c66837262a25b3f448d354a76";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  let apiKey = "494f3181eb1oe9bfae0t4f2214913d5b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  console.log(apiUrl);
   axios.get(apiUrl).then(showWeatherDetails);
 }
 
@@ -56,7 +75,23 @@ let days = [
   "Friday",
   "Saturday",
 ];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let year = now.getFullYear();
 let day = days[now.getDay()];
+let month = months[now.getMonth()];
 let hours = now.getHours();
 if (hours < 10) {
   hours = `0${hours}`;
@@ -66,7 +101,7 @@ if (minutes < 10) {
   minutes = `0${minutes}`;
 }
 let dayTime = document.querySelector("#timeDay");
-dayTime.innerHTML = `${day} / ${hours}:${minutes}`;
+dayTime.innerHTML = `${day} ${month} ${year} / ${hours}:${minutes}`;
 //submit search form
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
